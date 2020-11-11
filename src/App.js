7a25f90fa44885787291cd14778ea01f12048836
Shannon TJ <shannontj@shaw.ca>
2020-11-11 13:51:18 -0700

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns';
+import Setup from './tutorial/4-forms/setup/1-controlled-inputs';
 import Final from './tutorial/1-useState/setup/1-error-example';
 
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
       <Setup></Setup>
       {/* <Final></Final> */}
     </div>
--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,31 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
+  const [text, setText] = useState('');
+  const [isError, setIsError] = useState(false);
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  return (
+  <>
+
+
+  {/* display one of two things */}
+  {/* <h1>{text || 'if false: display john doe'}</h1> */}
+
+  {/* display something or nothing at all */}
+  {/* {text && <h1>if true: display hello world</h1>}
+  {!text && <h1>if false: display hello world</h1>} */}
+
+  <h1>{text || 'john doe'}</h1>  
+
+  <button className='btn' onClick={()=> setIsError(!isError)}>Toggle Error</button>
+
+  {isError && <h1>Error...</h1>}
+  {isError ? <p>There is an error...</p> : <div><h2>There is no error</h2></div>}
+
+  </>
+  );
 };
 
 export default ShortCircuit;
