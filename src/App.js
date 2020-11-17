a3eeb04782901896228e54e98536ae7a5858c832
Shannon TJ <shannontj@shaw.ca>
2020-11-16 18:29:21 -0700

    form basics

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import Setup from './tutorial/4-forms/setup/1-controlled-inputs';
-import Final from './tutorial/1-useState/setup/1-error-example';
+import React from "react";
+import Setup from "./tutorial/5-useRef/setup/1-useRef-basics";
+import Final from "./tutorial/1-useState/setup/1-error-example";
 
 function App() {
   return (
-    <div className='container'>
+    <div className="container">
       <Setup></Setup>
       {/* <Final></Final> */}
     </div>
   );
 }
 
-export default App
+export default App;
--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
@@ -6,28 +6,63 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [people, setPeople] = useState([]);
 
   const handleSubmit = (event) => {
     //default is to submit the form first thing, then refresh the page
     event.preventDefault();
-    console.log('hello');
-  }
+    if (firstName && email) {
+      const person = { id: new Date().getTime().toString(), firstName, email };
+      setPeople((currentPeople) => {
+        return [...currentPeople, person];
+      });
+      setFirstName("");
+      setEmail("");
+    } else {
+      console.log("empty values");
+    }
+  };
 
   return (
     <>
-    <article>
-      <form className='form' onSubmit={handleSubmit}>
-        <div className="form-control">
-          <label htmlFor="firstName">Name: </label>
-          <input type='text' id='firstName' name='firstName'></input>
-        </div>
-        <div className="form-control">
-          <label htmlFor="email">Email: </label>
-          <input type='text' id='email' name='email'></input>
-        </div>    
-        <button type='submit' onClick={handleSubmit}>Add Person</button>    
-      </form>
-    </article>
+      <article>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="firstName">Name: </label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            ></input>
+          </div>
+          <div className="form-control">
+            <label htmlFor="email">Email: </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+          </div>
+          <button type="submit" onClick={handleSubmit}>
+            Add Person
+          </button>
+        </form>
+        {people.map((person) => {
+          const { id, firstName, email } = person;
+          return (
+            <div className="item" key={id}>
+              <h4>{firstName} </h4>
+              <p>{email}</p>
+            </div>
+          );
+        })}
+      </article>
     </>
   );
 };
