e4bcea936146cb2a878fb6670cf02bfd233ce21e
Shannon TJ <shannontj@shaw.ca>
2020-11-20 15:29:21 -0700

    react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Setup from "./tutorial/10-prop-types/setup/index";
-import Final from "./tutorial/1-useState/setup/1-error-example";
+import Setup from "./tutorial/11-react-router/setup";
+// import Final from "./tutorial/1-useState/setup/1-error-example";
 
 function App() {
   return (
